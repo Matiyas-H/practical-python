61a8b2b952a69815ad161fe95faad261e55e2478
Matias_H <mattsis71@icloud.com>
2022-04-18 18:16:49 +0300

    exercise 1_5 bounce

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
-# bounce.py
-#
-# Exercise 1.5
+height = 100
+no_bounce = 1
+
+for i, v in enumerate(range(1, 11), start=1):
+    while no_bounce <= v:
+        height = height * 3/5
+        print(i, round(height, 4))
+        break
