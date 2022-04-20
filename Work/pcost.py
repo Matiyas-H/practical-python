cc2dc3aea2cc14567536c7b46765a9e7d2c7a920
Matias_H <mattsis71@icloud.com>
2022-04-20 18:27:15 +0300

    pcost exercise

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,7 +1,15 @@
+import csv
 f = open("Data/portfolio.csv")
+rows = csv.reader(f)
+headers = next(f)
 
-# headers = next(f)
 
-for line in f:
-    data = line["price"]
-    print(data)
+def calc_cost(x):
+    total_cost = 0
+    for row in x:
+        stoke_cost = row[2]
+        total_cost = total_cost + float(stoke_cost)
+    return round(total_cost, 3)
+
+
+print("total stoke cost: ", calc_cost(rows))
